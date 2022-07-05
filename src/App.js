2db8360905bd8a28313fd606ce1995fc50c80bac
Terbeche Mustapha <termus96@gmail.com>
2022-07-05 13:06:21 +0100

    add basic app skeleton

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import Nav from './components/Nav';
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-  
- 
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </Router>
   );
 }
 
