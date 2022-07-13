9d217399f68bedde89018757c988fa26594f040d
Terbeche Mustapha <termus96@gmail.com>
2022-07-13 18:55:01 +0100

    Dispatch action for categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './Categories.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
-const Categories = () => (
+const Categories = () => {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const check = () => dispatch(checkStatus());
 
-  <button type="button">
-    Check status
-  </button>
-);
+  return (
+    <div>
+      <button type="button" onClick={check}>
+        Check status
+      </button>
+      <h1>{status}</h1>
+    </div>
+  );
+};
 
 export default Categories;
