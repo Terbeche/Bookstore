8ef8057f58dede5c79920b073371111d1d1fedc0
Terbeche Mustapha <termus96@gmail.com>
2022-07-13 19:06:53 +0100

    Add some books to the initial state

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,7 +3,12 @@ import { v4 as uuidv4 } from 'uuid';
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-export default function reducer(state = [], action) {
+const defaultState = [
+  { title: 'The pragmatic programmer', author: 'Hunt Andrew & Thomas David', id: uuidv4() },
+  { title: 'Learn to Program', author: 'Chris Pine', id: uuidv4() },
+  { title: 'Learning JavaScript', author: 'Ethan Brown', id: uuidv4() },
+];
+export default function reducer(state = defaultState, action) {
   switch (action.type) {
     case ADD_BOOK:
       return [...state, action.payload];
