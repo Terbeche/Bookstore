daecbd1d8717a62f9e024b6eb322f5c9456ac56d
Terbeche Mustapha <termus96@gmail.com>
2022-07-14 21:47:11 +0100

    Remove commneted code and add MIT license to readme filr

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -17,7 +17,6 @@ const reducer = (state = initialState, action) => {
     case REMOVE_BOOK:
       // eslint-disable-next-line max-len
       return Object.fromEntries(Object.entries(state).filter((book) => book[0] !== payload.id));
-      // return state.filter(({ id }) => id !== payload.id);
     default:
       return state;
   }
